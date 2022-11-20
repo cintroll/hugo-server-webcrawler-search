02582d18acee587115771e23db09bdcb3d5285e0
Matheus CINTRA <matheus.goncalves@alumni.usp.br>
2022-11-20 19:48:09 -0300

    updated webcrawler

--- webcrawler/src/entrypoint.sh
+++ webcrawler/src/entrypoint.sh
@@ -9,24 +9,20 @@ bin/nutch fetch $s1
 bin/nutch parse $s1
 bin/nutch updatedb crawl/crawldb $s1
 
-bin/nutch generate crawl/crawldb crawl/segments -topN 1000
-s2=`ls -d crawl/segments/2* | tail -1`
+function updatedb {
+    bin/nutch generate crawl/crawldb crawl/segments -topN 1000
+    local s2=`ls -d crawl/segments/2* | tail -1`
 
-bin/nutch fetch $s2
-bin/nutch parse $s2
-bin/nutch updatedb crawl/crawldb $s2
+    bin/nutch fetch $s2
+    bin/nutch parse $s2
+    bin/nutch updatedb crawl/crawldb $s2
+}
 
-bin/nutch generate crawl/crawldb crawl/segments -topN 1000
-s3=`ls -d crawl/segments/2* | tail -1`
-echo $s3
-
-bin/nutch fetch $s3
-bin/nutch parse $s3
-bin/nutch updatedb crawl/crawldb $s3
-
-bin/nutch invertlinks crawl/linkdb -dir crawl/segments
+updatedb
+updatedb
 
 while true
 do
-    sleep 1
+    sleep 100
+    # updatedb
 done
